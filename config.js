63d1b670836baa5bc536c94e5442c945a84d418b
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2014-11-14 00:08:05 +0200

    Take port from env variable

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 var production = process.env.NODE_ENV === 'production';
 
+var productionPort = process.env.PORT || 80;
+var port = production ? productionPort : 8080;
+
 module.exports = {
-    port: production ? 80 : 8080
+    port: port;
 };
