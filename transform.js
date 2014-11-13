b29b165d425bbdf4b73b844d57706b0fcd1e4f33
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2014-11-13 23:21:18 +0200

    Add additional data of vehicles

--- transform.js
+++ transform.js
@@ -1,5 +1,29 @@
+var moment = require('moment');
+
+
 function transform(data) {
-    return data;
+    var vehicles = data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity;
+
+    return {
+        vehicles: vehicles.map(transformVehicle)
+    };
+}
+
+function transformVehicle(vehicle) {
+    var journey = vehicle.MonitoredVehicleJourney;
+
+    return {
+        id: journey.VehicleRef.value,
+        line: journey.LineRef.value,
+        latitude: journey.VehicleLocation.Latitude,
+        longitude: journey.VehicleLocation.Longitude,
+        bearing: journey.Bearing,
+        origin: journey.OriginName.value,
+        destination: journey.DestinationName.value,
+        operator: journey.OperatorRef.value,
+        direction: journey.DirectionRef.value,
+        validUntil: moment(vehicle.ValidUntilTime).toISOString()
+    };
 }
 
 module.exports = transform;
